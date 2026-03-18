85a15607bb097a4f67d0ac7856cfcab1f9679311
ScarFYTE <231283405+ScarFYTE@users.noreply.github.com>
2026-03-18 19:21:16 +0500

    Oh my Lord its working Finally

--- Comp_4300_IGT/WindowTest.cpp
+++ Comp_4300_IGT/WindowTest.cpp
@@ -2,11 +2,19 @@
 #include <optional> // Required for the new event system
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML Window Test");
+
+    const int Width = 1280;
+    const int Height = 720;
+    sf::RenderWindow window(sf::VideoMode({ Width, Height }), "SFML Window Test");
+    window.setFramerateLimit(60);
+
 
     sf::CircleShape shape(100.f);
 
-    shape.setPosition({ 100.f, 100.f });
+    float moveSpeedx = 1.0f;
+    float moveSpeedy = 1.0f;
+
+    shape.setPosition({ 300.f, 200.f });
     shape.setFillColor(sf::Color::Green);
     while (window.isOpen()) {
 
@@ -15,7 +23,18 @@ int main() {
                 window.close();
             }
         }
+        shape.setPosition({ shape.getPosition().x + moveSpeedx, shape.getPosition().y + moveSpeedy });
+        if (shape.getPosition().x + shape.getRadius() * 2 > Width || shape.getPosition().x < 0) {
+            moveSpeedx *= -1;
+            if (shape.getFillColor() == sf::Color::Green) shape.setFillColor(sf::Color::Blue);
+            else shape.setFillColor(sf::Color::Green);
+        }
+        else if (shape.getPosition().y + 2*shape.getRadius() > Height || shape.getPosition().y <0) {
+            moveSpeedy *= -1;
 
+            if (shape.getFillColor() == sf::Color::Green) shape.setFillColor(sf::Color::Blue);
+            else shape.setFillColor(sf::Color::Green);
+        }
         window.clear();
         window.draw(shape);
         window.display();
