ff45da1e6453e8646029f8c2a1f4d5731a9aa9cc
Syed Muhammad Shoaib Shah <231283405+ScarFYTE@users.noreply.github.com>
2026-05-11 03:02:02 +0500

    gbdgf

--- Comp_4300_IGT/Game.cpp
+++ Comp_4300_IGT/Game.cpp
@@ -12,9 +12,9 @@ Game::Game() {
 
 void Game::init() {
 
-	//levelQueue.enqueue("level1.txt");
+	levelQueue.enqueue("level1.txt");
 	//levelQueue.enqueue("Level2.txt");
-	levelQueue.enqueue("Level3.txt");
+	//levelQueue.enqueue("Level3.txt");
 
 	// 2. Dequeue the first level and load it
 	if (!levelQueue.isEmpty()) {
